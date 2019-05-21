9173307b73221473efaebce5f8a694dc0b7e330c
Joe Coyle <jcoyle37@gmail.com>
2019-05-21 11:14:22 -0400

    knex change to db; connection message

--- source/model/account.js
+++ source/model/account.js
@@ -1,13 +1,19 @@
 const db = require('../service/database');
 
+db.raw('select 1+1 as result').then(function() {
+  console.log('Database connection successful!');
+}).catch(function() {
+  console.log('There was an error connecting to the database.');
+});
+
 const create = (body) => {
-  return knex('accounts')
+  return db('accounts')
     .insert(body)
     .returning('*');
 };
 
 const find = (filter) => {
-  return knex
+  return db
     .select('*')
     .from('accounts')
     .where(filter.where)
